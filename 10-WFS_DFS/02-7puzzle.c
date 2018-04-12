9ef5d652247c89af33a367b26496a52fa937066c
Takahiro-Itazuri <takahiro.lab.1226@gmail.com>
2018-04-12 19:12:25 +0900

    add 7puzzle

--- 10-WFS_DFS/02-7puzzle.c
+++ 10-WFS_DFS/02-7puzzle.c
@@ -11,19 +11,27 @@ PATTERN *history;
 int history_count = 0;
 int queue_bottom;
 
+void show_pattern(char* pattern) {
+	int i;
+	for(i = 0; i < 8; i++) {
+		printf("%c ", pattern[i] ? pattern[i] + '0': ' ');
+		if(i % 4 == 3) printf("\n");
+	}
+}
+
 unsigned long make_hash(char* pattern) {
 	unsigned long hash;
 	int i;
 
 	hash = 0;
 	for(i = 0; i < 8; i++) {
-		hash |= ((unsigned long)pattern[7 - i]) << (i * 4);
+		hash |= ((unsigned long)(pattern[7 - i])) << (i * 4);
 	}
-
+	
 	return hash;
 }
 
-void pattern_from_hash(char* pattern, unsigned long hash) {
+void pattern_from_hash(char *pattern, unsigned long hash) {
 	int i;
 	for(i = 0; i < 8; i++) {
 		pattern[7 - i] = (char)((hash >> (i * 4)) & 0xf);
@@ -36,17 +44,16 @@ void save_history(char* pattern, int pattern_from) {
 
 	hash = make_hash(pattern);
 	for(i = 0; i < history_count; i++) {
-		if(history[i].hash == hash) return;
+		if(history[i].hash == hash) {
+			return;
+		}
 	}
 
 	history_count++;
 	history = (PATTERN*)realloc(history, sizeof(PATTERN) * (history_count));
-	if(history == NULL) {
-		printf("Shortage of Memory\n");
-		exit(EXIT_FAILURE);
-	}
+	if(history == NULL) exit(EXIT_FAILURE);
 	history[history_count - 1].hash = hash;
-	history[history_count - 1].pattern_from  = pattern_from;
+	history[history_count - 1].pattern_from = pattern_from;
 }
 
 int solve_7puzzle(void) {
@@ -58,13 +65,11 @@ int solve_7puzzle(void) {
 	while(queue_bottom != history_count) {
 		hash = history[queue_bottom].hash;
 
-		if(hash == 0x12345670) {
-			return 1;
-		}
-
+		if(hash == 0x12345670) return 1;
 		pattern_from_hash(pattern, hash);
+
 		for(blank_pos = 0; blank_pos < 8; blank_pos++) {
-			if(pattern[blank_pos] == 0) break;
+			if (pattern[blank_pos] == 0) break;
 		}
 
 		if(blank_pos > 3) {
@@ -73,24 +78,26 @@ int solve_7puzzle(void) {
 			save_history(pattern, queue_bottom);
 			pattern_from_hash(pattern, hash);
 		}
+		
 		if(blank_pos < 4) {
 			pattern[blank_pos] = pattern[blank_pos + 4];
 			pattern[blank_pos + 4] = 0;
 			save_history(pattern, queue_bottom);
 			pattern_from_hash(pattern, hash);
 		}
+		
 		if(blank_pos != 0 && blank_pos != 4) {
 			pattern[blank_pos] = pattern[blank_pos - 1];
 			pattern[blank_pos - 1] = 0;
 			save_history(pattern, queue_bottom);
 			pattern_from_hash(pattern, hash);
 		}
+
 		if(blank_pos != 3 && blank_pos != 7) {
 			pattern[blank_pos] = pattern[blank_pos + 1];
 			pattern[blank_pos + 1] = 0;
 			save_history(pattern, queue_bottom);
 		}
-
 		queue_bottom++;
 	}
 	return 0;
@@ -104,7 +111,7 @@ int main(void) {
 	save_history(pattern, -1);
 
 	if(solve_7puzzle() == 0) {
-		printf("Not Found.\n");
+		printf("No solution.\n");
 	}
 	else {
 		last = -1;
@@ -113,12 +120,9 @@ int main(void) {
 				i = history[i].pattern_from;
 			}
 			last = i;
-
+			
 			pattern_from_hash(pattern, history[last].hash);
-			for(i = 0; i < 8; i++) {
-				printf("%c ", pattern[i] ? pattern[i] + '0':' ');
-				if(i % 4 == 3) printf("\n");
-			}
+			show_pattern(pattern);
 			getchar();
 		}
 	}
